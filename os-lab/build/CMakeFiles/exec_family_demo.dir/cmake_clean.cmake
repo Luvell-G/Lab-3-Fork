6c36ecd178778279e8b6c3d96e273b682e267639
Luvell-G <trippmc16@yahoo.com>
2026-02-03 16:17:56 -0500

    Added build

---
+++ os-lab/build/CMakeFiles/exec_family_demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exec_family_demo.dir/src/exec_family_demo.c.o"
+  "CMakeFiles/exec_family_demo.dir/src/exec_family_demo.c.o.d"
+  "exec_family_demo"
+  "exec_family_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/exec_family_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ os-lab/build/CMakeFiles/exec_family_demo.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for exec_family_demo.
+# This may be replaced when dependencies are built.
